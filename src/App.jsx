d0e02d4af03a0804e654640d68b4ad69ecb42e84
Aviad Shmuel <vocaviad@gmail.com>
2022-11-14 19:37:33 +0200

    WIP - Using navbar in global app for testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 import './App.css';
 import ForgotPasswordPage from './components/ForgotPasswordPage/ForgotPasswordPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import SideNavBar from './components/SideNavBar/SideNavBar.tsx';
 
 function App() {
 
   return (
     <div className="App h-100">
-      <ForgotPasswordPage />
+      <div className="d-flex flex-row h-100">
+        <SideNavBar />
+        <ForgotPasswordPage />
+      </div>
     </div>
   );
 }
